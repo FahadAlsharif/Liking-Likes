bb1a868cc0207477c976e35f14f90c960a3810d3
FahadAlsharif <106029791+FahadAlsharif@users.noreply.github.com>
2022-05-31 00:01:42 +0300

    Add files via upload

---
+++ linking likes/Part 2/script.js
@@ -0,0 +1,15 @@
+var likess = 9
+function like () {
+    likess++
+    document.getElementById('totallike').innerText=likess
+}
+var likess2 = 12
+function like2 () {
+    likess2++
+    document.getElementById('totallike2').innerText=likess2
+}
+var likess3 = 9
+function like3 () {
+    likess3++
+    document.getElementById('totallike3').innerText=likess3
+}
